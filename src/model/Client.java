51fc8b5594103ef42b3bc10da9c90ec276f92f58
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2023-05-15 20:31:12 -0300

    add model client

---
+++ src/model/Client.java
@@ -0,0 +1,41 @@
+public class Client {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Cliente(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void showDatails() {
+        System.out.println("Cliente: " + name);
+        System.out.println("Endereço: " + address);
+        System.out.println("Telefone: " + phone);
+    }
+}
