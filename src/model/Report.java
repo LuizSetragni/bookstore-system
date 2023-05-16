19c1ac2543d2c2fae3ac26044e4308600c653e6c
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2023-05-15 23:54:06 -0300

    add model Report

---
+++ src/model/Report.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class Report {
+    private List<Book> inventory;
+    private List<Transaction> transactions;
+
+    public Report(List<Book> inventory, List<Transaction> transactions) {
+        this.inventory = inventory;
+        this.transactions = transactions;
+    }
+
+    public void generateReportInventory() {
+        System.out.println("Relatório de Inventário:");
+        for (Book book : inventory) {
+            System.out.println("Book: " + book.getTitle());
+            System.out.println("Autor: " + book.getAuthor());
+            // System.out.println("Quantidade disponível: " + book.getQuantity());
+            System.out.println("--------------------");
+        }
+    }
+
+    public void generateReportSales() {
+        System.out.println("Relatório de Vendas:");
+        for (Transaction transaction : transactions) {
+            System.out.println("Date: " + transaction.getDate());
+            System.out.println("Client: " + transaction.getClient().getName());
+            System.out.println("Books comprados:");
+            // for (Book book : transaction.getShoppingCart().getBooks()) {
+            // System.out.println("- " + book.getTitle() + " (" + book.getAuthor() + ")");
+            // }
+            System.out.println("Total: R$" + transaction.getTotalPurchase());
+            System.out.println("--------------------");
+        }
+    }
+}
